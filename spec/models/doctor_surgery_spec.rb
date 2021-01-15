1cfddaa75936ff2f1b54e8a8fff7697f2cd3f8be
Miquel <foymikek@gmail.com>
2021-01-15 09:31:23 -0700

    Add passing doctorsurgery relationship tests

--- spec/models/doctor_surgery_spec.rb
+++ spec/models/doctor_surgery_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DoctorSurgery, type: :model do
   describe 'relationships' do
-
+    it { should belong_to :doctor }
+    it { should belong_to :surgery }
   end
 end
